314cd1447aa5cdf9f3fca84665d4765d5cc2dcfc
Franz Gualambo <gualambo@gmail.com>
2021-12-16 15:21:50 -0500

    i

--- app/Validation/ClasecostoRules.php
+++ app/Validation/ClasecostoRules.php
@@ -5,7 +5,7 @@ use App\Models\ClasecostoModel;
 class ClasecostoRules
 {
     public function validateCodigo($codigo){
-        if((new ClasecostoModel())->where('codigo',$codigo)->first() == NULL){
+        if((new ClasecostoModel())->where('codigo',$codigo)->where('estado','1')->first() == NULL){
             return true;
         }else{
             return false;
