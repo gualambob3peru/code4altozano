7a4cdd7dc68644fb2f8913f7d27b846480f1e403
Franz Gualambo <gualambo@gmail.com>
2022-01-03 11:02:34 -0500

    jj

--- app/Views/Admin/oc/ver.php
+++ app/Views/Admin/oc/ver.php
@@ -1,4 +1,3 @@
-
 <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js" integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eNQATqgA/HdeKFVgA5v3S/cIrLF7QnIg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
@@ -12,27 +11,33 @@
     <div id="htmlpdf">
         <img src="images/logo1.png" alt="">
         <form method="POST" action="">
-        
+
             <div class="text-center mt-1 mb-4">
-                <h4>Orden de Compra - <?= $orden["codigo"] ?></h4>
+                <h4>
+                    <span class="<?= (($orden["estado"] == 1) ? "bg-success" : "bg-warning") ?>" style="padding: 7px;">
+
+                        Orden de Compra - <?= $orden["codigo"] ?>
+                    </span>
+                </h4>
+
             </div>
             <div class="text-center mt-4 mb-4 f14">
                 <div>
-                Fecha : <?= substr($orden["fecha"],0,10) ?>  
-                </div>    
-                <div>
-                Área : <?= $tipoOrden["descripcion"] ?>  
+                    Fecha : <?= substr($orden["fecha"], 0, 10) ?>
                 </div>
                 <div>
-                Tipo solicitud : <?= $tipoSolicitud["descripcion"] ?>
+                    Área : <?= $tipoOrden["descripcion"] ?>
+                </div>
+                <div>
+                    Tipo solicitud : <?= $tipoSolicitud["descripcion"] ?>
                 </div>
             </div>
-        
-        
-            <br>    
+
+
+            <br>
             <br>
             <h6>Datos del solicitado</h6>
-        
+
             <table class="table table-bordered f14">
                 <tr>
                     <td>Empresa</td>
@@ -51,15 +56,15 @@
                     <td><?php echo $personalJefe["nombres"] ?></td>
                 </tr>
             </table>
-        
-        
-        
+
+
+
             <div id="divCostos">
-        
+
                 <hr>
                 <h6>Datos del contrato</h6>
-                
-                <?php if ($cuenta==NULL): ?>
+
+                <?php if ($cuenta == NULL) : ?>
                     <table class="table table-bordered f14">
                         <thead>
                             <tr>
@@ -68,109 +73,121 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <?php foreach($orden_centros as $key=>$value): ?>
-                            <tr>
-                                <td><?= $value->codigo_centro." ".$value->descripcion_centro; ?></td>
-                                <td><?= $value->porcentaje; ?></td>
-                            </tr>
+                            <?php foreach ($orden_centros as $key => $value) : ?>
+                                <tr>
+                                    <td><?= $value->codigo_centro . " " . $value->descripcion_centro; ?></td>
+                                    <td><?= $value->porcentaje; ?></td>
+                                </tr>
                             <?php endforeach; ?>
                         </tbody>
                     </table>
-            
+
                 <?php else : ?>
                     <table class="table table-bordered f14">
-        
+
                         <tbody>
-                        
-                            
-                        <tr><td>Clase Costo</td>   <td><?php echo $cuenta->cc_codigo." ".$cuenta->cc_descripcion ?></td> </tr>
-                        <tr><td>Cuenta Nivel 1</td>   <td><?php echo $cuenta->c1_codigo." ".$cuenta->c1_descripcion ?></td> </tr>
-                        <tr><td>Cuenta Nivel 2</td>   <td><?php echo $cuenta->c2_codigo." ".$cuenta->c2_descripcion ?></td> </tr>
-                        <tr><td>Cuenta Nivel 3</td>   <td><?php echo $cuenta->c3_codigo." ".$cuenta->c3_descripcion ?></td> </tr>
-                        
-                    
+
+
+                            <tr>
+                                <td>Clase Costo</td>
+                                <td><?php echo $cuenta->cc_codigo . " " . $cuenta->cc_descripcion ?></td>
+                            </tr>
+                            <tr>
+                                <td>Cuenta Nivel 1</td>
+                                <td><?php echo $cuenta->c1_codigo . " " . $cuenta->c1_descripcion ?></td>
+                            </tr>
+                            <tr>
+                                <td>Cuenta Nivel 2</td>
+                                <td><?php echo $cuenta->c2_codigo . " " . $cuenta->c2_descripcion ?></td>
+                            </tr>
+                            <tr>
+                                <td>Cuenta Nivel 3</td>
+                                <td><?php echo $cuenta->c3_codigo . " " . $cuenta->c3_descripcion ?></td>
+                            </tr>
+
+
                         </tbody>
                     </table>
-            
-                <?php endif;?>
-        
-                
+
+                <?php endif; ?>
+
+
             </div>
             <hr>
             <h6>Objeto del contrato</h6>
-                    <div>
-                        <?php
-                            echo $orden["objeto"];
-                        ?>
-                    </div>
-        
-        
+            <div>
+                <?php
+                echo $orden["objeto"];
+                ?>
+            </div>
+
+
             <hr>
             <h6>Datos del contratista</h6>
-                <div>
-                    <table class="table table-bordered f14">
-                        <tbody>
-                            <tr>
-                                <td>Ejecutado</td>
-                                <td><?php echo $ejecutado["ruc"]. " " . $ejecutado["nombre"] ?></td>
-                            </tr>
-                            <tr>
-                                <td>Banco</td>
-                                <td><?php echo $banco->descripcion ?></td>
-                            </tr>
-                            <tr>
-                                <td>Nº Cuenta</td>
-                                <td><?php echo $banco->nroCuenta ?></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                      
-                    </div>
-        
-        
+            <div>
+                <table class="table table-bordered f14">
+                    <tbody>
+                        <tr>
+                            <td>Ejecutado</td>
+                            <td><?php echo $ejecutado["ruc"] . " " . $ejecutado["nombre"] ?></td>
+                        </tr>
+                        <tr>
+                            <td>Banco</td>
+                            <td><?php echo $banco->descripcion ?></td>
+                        </tr>
+                        <tr>
+                            <td>Nº Cuenta</td>
+                            <td><?php echo $banco->nroCuenta ?></td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
+
+
             <hr>
             <h6>Detalle del contrato</h6>
-        
-                <div>
-                    <table class="table table-bordered f14">
-        
-                        <tbody>                
-                            <?php foreach($ordenDetalle as $key=>$value): ?>
+
+            <div>
+                <table class="table table-bordered f14">
+
+                    <tbody>
+                        <?php foreach ($ordenDetalle as $key => $value) : ?>
                             <tr>
                                 <td><?php echo $value->descripcion ?></td>
                                 <td><?php echo $value->monto ?></td>
                             </tr>
-                            <?php endforeach; ?>
-                            <tr>
-                                <td class="text-end">Total</td>
-                                <td>
-                                    <?php echo $orden["importe"]?>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="text-end">Moneda</td>
-                                <td><?= $moneda["descripcion"] ?></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-           
+                        <?php endforeach; ?>
+                        <tr>
+                            <td class="text-end">Total</td>
+                            <td>
+                                <?php echo $orden["importe"] ?>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="text-end">Moneda</td>
+                            <td><?= $moneda["descripcion"] ?></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
             <hr>
             <h6>Referencia</h6>
-                <div>
-                    <?php echo $orden["referencia"] ?>
-                </div>
+            <div>
+                <?php echo $orden["referencia"] ?>
+            </div>
             <h6>Documentos</h6>
-                <?php foreach($images as $key=>$value): ?>
-                    <div class="f14">
-                        Doc <?= $key+1 ?>: <a target="_blank" href="uploads/<?= $value->idOrden ?>/<?= $value->imagen?>">uploads/<?= $value->idOrden ?>/<?= $value->imagen?></a>
-                    </div>
-                <?php endforeach; ?>                 
-        
+            <?php foreach ($images as $key => $value) : ?>
+                <div class="f14">
+                    Doc <?= $key + 1 ?>: <a target="_blank" href="uploads/<?= $value->idOrden ?>/<?= $value->imagen ?>">uploads/<?= $value->idOrden ?>/<?= $value->imagen ?></a>
+                </div>
+            <?php endforeach; ?>
+
             <br>
             <br>
             <br>
-        
+
             <br>
             <br>
             <br>
@@ -180,15 +197,20 @@
 </div>
 
 <script>
-    $(function(){
+    $(function() {
         let codigo = '<?php echo $orden["codigo"] ?>';
-        $("#descargar").click(function(){
+        $("#descargar").click(function() {
             var element = document.getElementById('htmlpdf');
             var opt = {
-                margin:       1,
-                filename:     codigo+'.pdf',
-                image:        { type: 'jpeg', quality: 1 },
-                html2canvas:  { scale: 1 },
+                margin: 1,
+                filename: codigo + '.pdf',
+                image: {
+                    type: 'jpeg',
+                    quality: 1
+                },
+                html2canvas: {
+                    scale: 1
+                },
             };
 
             html2pdf().set(opt).from(element).save();
@@ -196,4 +218,3 @@
         });
     });
 </script>
-
