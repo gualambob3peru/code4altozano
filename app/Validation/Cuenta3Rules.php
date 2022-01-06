140aa56e8ebdcfa8aaac7d42e35e40b121acc04e
Franz Gualambo <gualambo@gmail.com>
2022-01-06 15:10:41 -0500

    yy

--- app/Validation/Cuenta3Rules.php
+++ app/Validation/Cuenta3Rules.php
@@ -5,7 +5,7 @@ use App\Models\Cuenta3Model;
 class Cuenta3Rules
 {
     public function validateCodigoCuenta3($codigo){
-        if((new Cuenta3Model())->where('codigo',$codigo)->first() == NULL){
+        if((new Cuenta3Model())->where('codigo',$codigo)->where('estado','1')->first() == NULL){
             return true;
         }else{
             return false;
