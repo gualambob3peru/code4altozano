83e05a0409bb68fc72920ab1cd2ae92fe8a6175c
Franz Gualambo <gualambo@gmail.com>
2022-01-07 16:38:06 -0500

    gg

--- app/Views/templates/template2.php
+++ app/Views/templates/template2.php
@@ -61,7 +61,7 @@
     ?>
     <nav class="navbar navbar-expand-lg miNav navbar-dark" style="height: 39px">
         <div class="container-fluid">
-            <a class="navbar-brand" href="admin">Dashboard</a>
+        <!--     <a class="navbar-brand" href="admin">Dashboard</a> -->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
