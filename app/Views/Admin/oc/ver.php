f894b287a30badad766c7e51c1c78df7b9a4a491
Franz Gualambo <gualambo@gmail.com>
2022-02-02 13:58:03 -0500

    kkjh

--- app/Views/Admin/oc/ver.php
+++ app/Views/Admin/oc/ver.php
@@ -1,13 +1,21 @@
 <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js" integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eNQATqgA/HdeKFVgA5v3S/cIrLF7QnIg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 
 <div class="container">
 
     <div class="text-end">
         <button id="descargar" class="btn btn-danger mb-4 text-white"><i class="bi bi-file-earmark-pdf"></i> PDF</button>
+        <?php
+
+if($_SESSION["personal"]["idCargo"]=="1"){
+    echo " <button data-bs-toggle='tooltip' data-bs-placement='top' title='Aprobar Orden' elId='".$orden["id"] ."' class='text-white btn btn-success mb-4  btnAprobar'><i class='bi bi-check-lg'></i> Aprobar Orden</button>";
+}
+?>
     </div>
+    
     <div id="htmlpdf">
         <img src="images/logo1.png" alt="">
         <form method="POST" action="">
@@ -19,6 +27,7 @@
                         Orden de Compra - <?= $orden["codigo"] ?>
                     </span>
                 </h4>
+              
 
             </div>
             <div class="text-center mt-4 mb-4 f14">
@@ -198,8 +207,35 @@
     </div>
 </div>
 
+<div class="modal" tabindex="-1" id="modalAprobar">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">¿Desea aprobar OC?</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                <a class="btn btn-primary" id="btnOkAprobar">Aceptar</a>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script>
     $(function() {
+        let modalAprobar = new bootstrap.Modal(document.getElementById('modalAprobar'), {});
+        let btnAprobarAll = document.getElementsByClassName('btnAprobar');
+        for(let i=0; i<btnAprobarAll.length;i++){
+            btnAprobarAll[i].onclick = function(){
+                let id = this.getAttribute('elId');
+
+                document.getElementById('btnOkAprobar').setAttribute('href','admin/oc/aprobar/'+id);
+                modalAprobar.show();
+
+            }
+        }
+
         let codigo = '<?php echo $orden["codigo"] ?>';
         $("#descargar").click(function() {
             var element = document.getElementById('htmlpdf');
